4f1625c4244a38aa0036e0f515f15714f59f1260
usuario <sdorgod3108@fp.iesromerovargas.com>
2025-01-27 09:16:11 +0100

    1

--- FinalFantasyProject/FinalFantasyProject/settings.py
+++ FinalFantasyProject/FinalFantasyProject/settings.py
@@ -124,5 +124,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGIN_URL = 'accounts/login/'
+LOGIN_REDIRECT_URL = '/games/'
+LOGOUT_REDIRECT_URL = '/games/'
--- FinalFantasyProject/games/views.py
+++ FinalFantasyProject/games/views.py
@@ -7,6 +7,8 @@ from django.http import HttpResponse
 
 from games.models import Game, Character, Genre, Job
 
+
+
 @login_required
 def index(request):
 
@@ -16,18 +18,22 @@ def index(request):
 
 
 @login_required
+
 def game_details(request, game_id):
 
     game = Game.objects.get(pk=game_id)
     context = {'game' : game }
     return render(request, 'games/game_details.html', context)
+
 @login_required
+
 def character_details(request, character_id):
     character = get_object_or_404(Character, id=character_id)
     context = {'character': character}
     return render(request, 'games/character_details.html', context)
 
 @login_required
+
 def character_list(request):
 
     characters = Character.objects.all()
@@ -35,12 +41,14 @@ def character_list(request):
     return render(request, 'games/character_list.html', context)
 
 @login_required
+
 def genre_list(request):
     genres = Genre.objects.all()
     context = {'genres' : genres}
     return render(request,'games/genre_list.html',context)
 
 @login_required
+
 def job_list(request):
     jobs = Job.objects.all()
     context = {'jobs' : jobs}
